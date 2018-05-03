00e54ae93c7f0bad2d269e1c752b685aea2b21d1
unknown <rek177@yahoo.co.jp>
2018-05-03 22:09:13 +0900

    201805032209

---
+++ helloworld.rb
@@ -0,0 +1,34 @@
+
+=begin
+コメント続けてかける
+;で命令を一行に2つかける！
+=end
+
+
+print "hello world"
+puts "hello world" #これの後に改行をいれる
+p "hello world" #デバック用　文字列だとかを表示してくれる文字列なら""がつく
+
+
+# 変数
+# 英小文字か_で始めないといけない。定義は必要ない
+
+msg = "hello world"
+puts msg
+
+msg = "hello world again"
+puts msg
+
+# 定数
+# 英大文字で始めないといけない。定義は必要ない
+
+VERSION = 1.1
+puts VERSION
+
+VERSION = 1.2
+puts VERSION
+
+
+
+
+
